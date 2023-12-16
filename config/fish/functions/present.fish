ebad3a87a599d8d7fd193f66f60abe918c8398c9
Noah Syrkis <noah@syrkis.com>
2023-12-16 15:29:15 -0300

    latex slide template

--- config/fish/functions/present.fish
+++ config/fish/functions/present.fish
@@ -1,19 +1,50 @@
 function present
+    if test (count $argv) -lt 1
+        echo "Usage: present <file>"
+        return 1
+    end
+
     set file $argv[1]
-    set template ~/code/template/slide.tex
-    mkdir output
-    pandoc $file -t beamer --template=$template --biblatex -o output/output.tex
+    set template /Users/syrkis/.config/fish/functions/slide.tex
+
+    if not test -e $file
+        echo "File '$file' not found."
+        return 1
+    end
+
+    # Extract title and process it
+    set title (sed -n 's/^title: \(.*\)$/\1/p' $file | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr ' ' '_')
+
+    if test -z "$title"
+        echo "No title found in '$file'."
+        return 1
+    end
+
+    mkdir -p output
+
+    if not pandoc $file --template=$template -t beamer --slide-level=3 --metadata=link-citations:true --biblatex -o output/output.tex
+        echo "Pandoc conversion failed."
+        return 1
+    end
+
     cd output
-    pdflatex output.tex
-    biber output.bcf
-    pdflatex output.tex
-    pdflatex output.tex
-    mv output.pdf ../output.pdf
-    cd ..
-    rm -rf output
-    open output.pdf
+
+    cp ../*.{png,jpg,jpeg} .
+
+
+    for img in *.png *.jpg *.jpeg
+        if test -e $img
+            convert "$img" -resize 1920x1080 -quality 75 "$img"
+        end
+    end
+
+    pdflatex output.tex && biber output.bcf && pdflatex output.tex && pdflatex output.tex
+
+    mv output.pdf "../$title.pdf"
+
+    cd ../
+
+    echo "Presentation created: $title.pdf"
+    open "$title.pdf"
 end
 
-
-
-# LaTeX commands
--- config/fish/functions/todo.fish
+++ config/fish/functions/todo.fish
@@ -1,41 +1,14 @@
 function todo
-    # Navigate to the repository
-    set -l repo_path ~/text/todo
-    cd $repo_path
+    cd ~/code/todo
 
-    set -l remote_latest (git ls-remote origin HEAD | awk '{print $1}')
-    set -l local_latest (git rev-parse HEAD)
-
-    if test "$remote_latest" != "$local_latest"
-        if not git pull
-            echo "Failed to pull latest changes. Exiting."
-            return 1
-        end
-    end
-
-    # Open noah.txt in vim
     vim noah.txt
 
-    # Check for changes in noah.txt specifically
-    if test -n (git status noah.txt --porcelain)
-        # Add only noah.txt to avoid committing unintended changes
-        git add noah.txt
-
-        # Commit and push the changes
-        if git commit -m "Update noah.txt"
-            echo "Changes committed. Pushing to remote."
-            if not git push
-                echo "Failed to push changes. Exiting."
-                return 1
-            end
-        else
-            echo "No changes to commit."
-        end
-    else
+    if git diff --exit-code noah.txt
         echo "No changes in noah.txt."
+    else
+        git add noah.txt
+        git commit -m "Update noah.txt" && git push || echo "Push failed."
     end
 
-    # Return to the original directory
     cd -
 end
-
