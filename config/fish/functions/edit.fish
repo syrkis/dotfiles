2fcee8d002cba8a74c4f52accbb6091434000784
Noah Syrkis <noah@syrkis.com>
2024-08-04 07:25:45 +0200

    auto push

--- config/fish/functions/edit.fish
+++
@@ -1,8 +0,0 @@
-function edit
-    # file is the first argument
-    set file $argv[1]
-    # file path is current directory + file
-    set file_path (pwd)/$file
-    # Open Alacritty in fullscreen mode with nvim editing the file
-    alacritty --option window.startup_mode=Fullscreen -e nvim $file_path
-end
